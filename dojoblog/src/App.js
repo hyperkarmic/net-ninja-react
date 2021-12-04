bd312bbb13d639dabaeb8960aab182e7a4780001
hyperkarmic <randy_pan@yahoo.com>
2021-12-04 21:17:55 +0000

    the classic react app-component nesting behaviour demonstrated

--- dojoblog/src/App.js
+++ dojoblog/src/App.js
@@ -1,18 +1,13 @@
 import './App.css'
+import Navbar from './Navbar.js'
+import Home from './Home.js'
 
 function App() {
-  const title = 'welcome to the new blog!!!'
-  const likes = 50
-  const link = 'http://www.google.com'
   return (
     <div className='App'>
+      <Navbar />
       <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked {likes} Times</p>
-        <p>Hello Ninjas</p>
-        <p>[1,2,3,4,5]</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>google site</a>
+        <Home />
       </div>
     </div>
   )
