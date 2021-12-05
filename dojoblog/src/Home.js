7ee9dfea7a7ffe3b84aaabfd3be40609285ed544
hyperkarmic <randy_pan@yahoo.com>
2021-12-05 00:04:37 +0000

    click events.  taking functions.  taking anonymous functions.  can take event object as as argument.  don't add to functions in click event if named.  Lot's of weird closure hoodoo.  THIS LECTURE NEEDS REVIEWING

--- dojoblog/src/Home.js
+++ dojoblog/src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('hello ninjas!!!!', e.target)
+  }
+
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name)
+  }
+
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me!!!</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain('Simon', e.target)
+        }}
+      >
+        Click me again!!!
+      </button>
     </div>
   )
 }
