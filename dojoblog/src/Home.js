9ebcb5132ce74d7de688c153a9b127ae06f6f4cb
hyperkarmic <randy_pan@yahoo.com>
2021-12-05 23:44:49 +0000

    this shows how to use a modified version of the javascript map function to render data using javascript.  Note numerable quality ie id is used as the key quality....also css updated....

--- dojoblog/src/Home.js
+++ dojoblog/src/Home.js
@@ -1,21 +1,20 @@
 import { useState } from 'react'
 
 const Home = () => {
-  const [age, setAge] = useState(25)
-  const [name, setName] = useState('Mario')
-
-  const handleClick = () => {
-    setName('luigi')
-    setAge(30)
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'hate Gervais', body: "he's a twit", author: 'Si', id: 1 },
+    { title: 'hate TV', body: "it's rubbish", author: 'Si', id: 2 },
+    { title: 'hate BBC', body: 'so patronising', author: 'Si', id: 3 },
+    { title: 'hate custard', body: "it's yellow", author: 'Si', id: 4 },
+  ])
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me!!!</button>
+      {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
