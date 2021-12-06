ea05ba0f8e60db9f7c5c563c6bdf2c82138841b5
hyperkarmic <randy_pan@yahoo.com>
2021-12-06 01:05:11 +0000

    lecture 12

--- dojoblog/src/Home.js
+++ dojoblog/src/Home.js
@@ -3,7 +3,7 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'hate Gervais', body: "he's a twit", author: 'Si', id: 1 },
+    { title: 'hate Gervais', body: "he's a twit", author: 'mario', id: 1 },
     { title: 'hate TV', body: "it's rubbish", author: 'Si', id: 2 },
     { title: 'hate BBC', body: 'so patronising', author: 'Si', id: 3 },
     { title: 'hate custard', body: "it's yellow", author: 'Si', id: 4 },
@@ -11,6 +11,10 @@ const Home = () => {
   return (
     <div className='home'>
       <BlogList blogs={blogs} title='All Blogs' />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === 'mario')}
+        title='Warluigis Blogs'
+      />
     </div>
   )
 }
