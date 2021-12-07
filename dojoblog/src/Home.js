4f8d7fb225d0fcdd45bfe39c78d945e9fc64a82f
hyperkarmic <randy_pan@yahoo.com>
2021-12-07 17:46:38 +0000

    functions can be used as props.  Delete handlers should be coded in home component.  Filter functions give delete button capability.  useState hook allows us to use new array as the state object that components get their information off

--- dojoblog/src/Home.js
+++ dojoblog/src/Home.js
@@ -8,9 +8,15 @@ const Home = () => {
     { title: 'hate BBC', body: 'so patronising', author: 'Si', id: 3 },
     { title: 'hate custard', body: "it's yellow", author: 'Si', id: 4 },
   ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id)
+    setBlogs(newBlogs)
+  }
+
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs' />
+      <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === 'mario')}
         title='Warluigis Blogs'
